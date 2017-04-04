dcbc5f2ba34b117038546be1b774d1af72e72742
David J <davidjosuedjdj@gmail.com>
2017-04-04 15:25:24 -0500

    Add sessions

--- server.js
+++ server.js
@@ -2,16 +2,40 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
+
+var session = require('express-session');
+var uuid = require('node-uuid');
+var MongoStore = require('connect-mongo')(session);
 
 var path = require('path');
 app.use(express.static(path.join(__dirname, './build')));
+
+app.use(session({
+  genid: function(req) {
+    return uuid.v1();
+  },
+  store: new MongoStore({
+    url: 'mongodb://localhost/bimb-web-app',
+    autoRemove: 'native',
+    touchAfter: 24 * 3600 // time period in seconds || = 24 hours; Let the session be updated once every 24 hours
+  }),
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000 // log out after 1 month (days * hours * minutes * seconds * milliseconds)
+  }
+}));
 
 //require mongoose
 require('./server/config/mongoose.js');
 
 //requiere routes
 require('./server/config/routes.js')(app);
-app.listen(8000, function() {
-  console.log("Server running on port 8000")
+
+var port = (process.env.NODE_PORT || 8000);
+app.listen(port, function() {
+  console.log("Server running on port " + port);
 })

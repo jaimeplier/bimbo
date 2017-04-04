76e142d2217014834806a7443bd461f8bcce1171
David J <davidjosuedjdj@gmail.com>
2017-04-04 07:23:49 -0500

    Eject react-create-app webpack

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import './styles/main.scss';
+
 class App extends Component {
   render() {
     return (

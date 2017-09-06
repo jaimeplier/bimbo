727474ea3ff6a1c6a5deb04ddc9de7360b802c48
David J <davidjosuedjdj@gmail.com>
2017-09-06 15:18:54 -0500

    Remove auth for action plans api

--- server/config/routes.js
+++ server/config/routes.js
@@ -38,7 +38,7 @@ module.exports = function(app) {
 
 
   app.get('/api/action-plans/kpis', authM, ActionPlansCtr.getKPIs);
-  app.get('/api/action-plans', authM, ActionPlansCtr.get);
+  app.get('/api/action-plans', ActionPlansCtr.get);
 
 
   app.all('/api/*', function(req, res, next) {
